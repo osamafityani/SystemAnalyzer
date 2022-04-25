3d91e6882a64d8ceac025abd24ec032b44890778
osamafityani <osamafitiani2001@gmail.com>
2022-04-25 17:24:35 +0300

    added steady state error analysis and design

---
+++ steadyStateError.m
@@ -0,0 +1,49 @@
+function [stable,step, ramp, parabolic] = steadyStateError(G,H)
+%{
+find the steady state error of any closed loop system due to step, ramp,
+and parablic input
+%}
+
+
+
+% find the characterstic equation
+sys = G / (1 + G * H);
+sys = minreal(sys);
+[~, originalDen] = tfdata(sys);
+charEquation = originalDen{1};
+stable = RHC(charEquation);
+
+if stable == 1
+    unityFeedbackSys = G / (1 + G *(H - 1));
+    unityFeedbackSys = minreal(unityFeedbackSys);
+    [unityNum, unityDen] = tfdata(unityFeedbackSys);    
+    systemType = sum(roots(unityDen{1}) == 0) - sum(roots(unityNum{1}) == 0);
+    errorConstant = unityNum{1}(end) / unityDen{1}(length(unityDen{1}) - systemType);
+
+    switch systemType
+        case 0
+            step = 1 / (1 + errorConstant);
+            ramp = inf;
+            parabolic = inf;
+        case 1
+            step = 0;
+            ramp = 1 / errorConstant;
+            parabolic = inf;
+        case 2
+            step = 0;
+            ramp = 0;
+            parabolic = 1 / errorConstant;
+        otherwise
+            step = 0;
+            ramp = 0;
+            parabolic = 0;
+    end
+    
+else
+    disp("System is either marginally stable or unstable!");
+    step = inf;
+    ramp = inf;
+    parabolic = inf;
+end
+end
+
---
+++ steadyStateErrorDesign.m
@@ -0,0 +1,56 @@
+function [step, ramp, parabolic, kRange] = steadyStateErrorDesign(forwardNum, forwardDen,backwardNum, backwardDen)
+% find the steady state error with respect to K
+syms k real
+syms x
+% find the characterstic equation
+
+symG = poly2sym(forwardNum)/ poly2sym(forwardDen);
+symH = poly2sym(backwardNum)/ poly2sym(backwardDen);
+symSys = (symG)/(1 + symG * symH);
+symSys = simplify(symSys);
+[~, den] = numden(symSys);
+charEquation = coeffs(den, x);
+kRange = RHCDesign(charEquation);
+
+k = 1;
+forwardNum = subs(forwardNum);
+forwardNum = double(forwardNum);
+
+syms k real
+    
+G = tf(forwardNum, forwardDen);
+H = tf(backwardNum, backwardDen);
+
+if length(kRange) == 1
+    unityFeedbackSys = G / (1 + G *(H - 1));
+    unityFeedbackSys = minreal(unityFeedbackSys);
+    [unityNum, unityDen] = tfdata(unityFeedbackSys);    
+    systemType = sum(roots(unityDen{1}) == 0) - sum(roots(unityNum{1}) == 0);
+    errorConstant = unityNum{1}(end) / unityDen{1}(length(unityDen{1}) - systemType) * k;
+
+    switch systemType
+        case 0
+            step = 1 / (1 + errorConstant);
+            ramp = inf;
+            parabolic = inf;
+        case 1
+            step = 0;
+            ramp = 1 / errorConstant;
+            parabolic = inf;
+        case 2
+            step = 0;
+            ramp = 0;
+            parabolic = 1 / errorConstant;
+        otherwise
+            step = 0;
+            ramp = 0;
+            parabolic = 0;
+    end
+    
+else
+    disp("System is either marginally stable or unstable for all values of K!");
+    step = inf;
+    ramp = inf;
+    parabolic = inf;
+end
+end
